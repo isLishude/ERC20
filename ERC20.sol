c1b66dd140e243174be0210b43c2cc179c01da21
Shude Li <lishude@aliyun.com>
2018-12-20 15:41:15 +0800

    feat: update solidity pragma to 0.5.2

--- ERC20.sol
+++ ERC20.sol
@@ -1,19 +1,18 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.5.2;
 
-contract ERC20Token {
-
-    string public name = "ERC20Token"; 
-    string public symbol = "ERC20"; 
-    uint256 public decimals = 8;
+contract TronToken {
+    string public name = "ERC20Token";
+    string public symbol = "ERC20Token";    
+    uint256 public decimals = 6;
 
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
-    uint256 public totalSupply = 10 ** (18 + 8);
+    uint256 public totalSupply = 0;
     bool public stopped = false;
 
-    uint256 constant valueFounder = 10 ** (18 + 8);
-    address owner = 0x0;
+    uint256 constant valueFounder = 100000000000000000;
+    address owner = 0x0000000000000000000000000000000000000000;
 
     modifier isOwner {
         assert(owner == msg.sender);
@@ -26,15 +25,15 @@ contract ERC20Token {
     }
 
     modifier validAddress {
-        assert(0x0 != msg.sender);
+        assert(0x0000000000000000000000000000000000000000 != msg.sender);
         _;
     }
 
-    constructor (address _addressFounder) public {
+    constructor(address _addressFounder) public {
         owner = msg.sender;
         totalSupply = valueFounder;
         balanceOf[_addressFounder] = valueFounder;
-        emit Transfer(0x0, _addressFounder, valueFounder);
+        emit Transfer(0x0000000000000000000000000000000000000000, _addressFounder, valueFounder);
     }
 
     function transfer(address _to, uint256 _value) public isRunning validAddress returns (bool success) {
@@ -57,19 +56,30 @@ contract ERC20Token {
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public isRunning validAddress returns (bool success) {
+    function approve(address _spender, uint256 _value)public isRunning validAddress returns (bool success) {
         require(_value == 0 || allowance[msg.sender][_spender] == 0);
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function stop() public isOwner {
+    function stop()public isOwner {
         stopped = true;
     }
 
-    function start() public isOwner {
+    function start()public isOwner {
         stopped = false;
+    }
+
+    function setName(string memory _name) public isOwner {
+        name = _name;
+    }
+
+    function burn(uint256 _value)public {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[0x0000000000000000000000000000000000000000] += _value;
+        emit Transfer(msg.sender, 0x0000000000000000000000000000000000000000, _value);
     }
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
